bf034d954eccb412b740cef4a6b2b5ede856ca4b
kissco_dhlee <dhlee@kissco.co.jp>
2024-05-17 11:25:48 +0900

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+// useState : 기본 array [값value, 값변화modify]
+// useEffect : 한번만 실행하고 싶은 function () => {once execute, watch}
+function App() {
+  return (
+    <div></div>
+  );
+}
+
+export default App;
