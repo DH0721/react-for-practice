14051eedd497b75c6b94f498c8ad08958c5257df
kissco_dhlee <dhlee@kissco.co.jp>
2024-06-11 17:29:13 +0900

    react-build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import * as React from "react";
-import { createRoot } from "react-dom/client";
-// import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
@@ -15,20 +13,9 @@ function App() {
             path: "/detail",
             element: <Detail />,
         },
-        // {
-        //     path: "about",
-        //     element: (
-        //         <div>
-        //             <h1>Hello world</h1>
-        //             <Link to="/about">About us</Link>
-        //         </div>
-        //     ),
-        // },
     ]);
 
     return <RouterProvider router={router} />;
 }
-
-createRoot(document.getElementById("root")).render(<App />);
 
 export default App;
